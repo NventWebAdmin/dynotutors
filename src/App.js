d292df14187ca7eaebc0b8d7f4e2026f317f06ad
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2021-03-02 10:36:38 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacttttt
+          Learn Reactttt
         </a>
       </header>
     </div>
